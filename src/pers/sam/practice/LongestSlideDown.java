f336adc510cae474d931cb3b486f416107146bda
sum41 <denglee123@126.com>
2016-10-14 23:17:06 +0800

    2016年10月14日23:16:41
    Initial

---
+++ src/pers/sam/practice/LongestSlideDown.java
@@ -0,0 +1,23 @@
+package pers.sam.practice;
+
+
+/**
+ * 
+ * @author SUM
+ * https://www.codewars.com/kata/551f23362ff852e2ab000037/train/java
+ * 
+ */
+public class LongestSlideDown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public static int longestSlideDown(int[][] pyramid) {
+        // Code Goes Here..
+    	
+    	return 0;
+    }
+
+}
